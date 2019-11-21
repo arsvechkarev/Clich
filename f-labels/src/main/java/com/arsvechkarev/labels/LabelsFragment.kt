f3314ffb1d81b5d7d3008687074438f9f101649a
Treft29 <arsvechkarev@gmail.com>
2019-11-21 23:11:33 +0300

    Adding fragment labels (Add other files)

---
+++ f-labels/src/main/java/com/arsvechkarev/labels/LabelsFragment.kt
@@ -0,0 +1,8 @@
+package com.arsvechkarev.labels
+
+import com.arsvechkarev.core.BaseFragment
+
+class LabelsFragment : BaseFragment() {
+  override val layoutId: Int = R.layout.fragment_labels
+  
+}
---
+++ f-labels/src/main/java/com/arsvechkarev/labels/list/LabelsAdapter.kt
@@ -0,0 +1,33 @@
+package com.arsvechkarev.labels.list
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.arsvechkarev.core.domain.model.Label
+import com.arsvechkarev.core.extensions.inflate
+import com.arsvechkarev.labels.R
+import kotlinx.android.synthetic.main.item_label.view.textLabel
+
+class LabelsAdapter(
+  private val clickListener: (Label) -> Unit = {}
+) : RecyclerView.Adapter<LabelsAdapter.LabelViewHolder>() {
+  
+  private var data: List<Label> = ArrayList()
+  
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LabelViewHolder {
+    return LabelViewHolder(parent.inflate(R.layout.item_label))
+  }
+  
+  override fun getItemCount() = data.size
+  
+  override fun onBindViewHolder(holder: LabelViewHolder, position: Int) {
+    holder.bind(data[position], clickListener)
+  }
+  
+  class LabelViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(item: Label, clickListener: (Label) -> Unit) {
+      itemView.setOnClickListener { clickListener(item) }
+      itemView.textLabel.text = item.name
+    }
+  }
+}
