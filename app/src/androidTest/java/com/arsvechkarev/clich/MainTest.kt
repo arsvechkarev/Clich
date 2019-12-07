678aa32c93ee14853dec7a1af86a9bb2337faf1a
Treft29 <arsvechkarev@gmail.com>
2019-12-07 23:41:43 +0300

    Minor refactoring

--- app/src/androidTest/java/com/arsvechkarev/clich/MainTest.kt
+++ app/src/androidTest/java/com/arsvechkarev/clich/MainTest.kt
@@ -16,6 +16,15 @@ class MainTest {
   @get:Rule
   val activityRule = ActivityTestRule(MainActivity::class.java)
   
+  /**
+   * Plan of test:
+   *
+   * 1. Click to new word button
+   * 2. Create word
+   * 3. Check if it is displayed in the recycler
+   * 4. Click on this item
+   * 5. Go to info fragment and check if data displayed correctly
+   */
   @Test
   fun creating_new_word_and_checking_whether_it_is_displayed_after_that() {
     onScreen<TheWordsListScreen> {
